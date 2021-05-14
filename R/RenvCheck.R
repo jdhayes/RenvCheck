4dc7afb729b9bc48c365b2793cf37f87ce2450f8
Jordan Hayes <hayesjordan@gmail.com>
2021-05-14 10:25:23 -0700

    Converted to R6 class

--- R/RenvCheck.R
+++ R/RenvCheck.R
@@ -3,129 +3,157 @@
 #library(methods)
 #options(warn=-1)
 
-# Define method for calling each check function
-run_checks <- function(object){
-    maxwidth <- 15
-    cat ("\n R Environment Test Results\n")
-    for (i in 1:length(object)){
-        name <- substr(names(object)[i], 7, nchar(names(object)[i]))
-        name <- sub("_"," ",paste0(toupper(substring(name, 1,1)), substring(name, 2)))        
-        padding <- rep(" ", maxwidth-nchar(name))
-        cat (paste("\t",name)); cat(paste(padding,collapse='')); cat("\t: ");
-        cat(paste(as.character(do.call((object)[[i]], list())), "\n"));
-    }
-    cat ("\n")
-}
+library(R6)
 
-# Define S3 class, A better way to handle dynamic calling of functions
-myRenvCheck <- structure(list(), class="RenvCheck")
+# Define R6 class to hold functions to be dynamically called
+RenvCheck <- R6Class("RenvCheck",
+    public = list(
+        initialize = function(){
+            maxwidth <- 21
+            cat ("\n R Environment Test Results\n")
+            # Iterate through function names
+            for (i in 1:length(names(self))){
+                # Get function name
+                fname <- names(self)[[i]]
+                
+                # Exclude some built in functions
+                if ( length(grep('^\\.',fname)) != 0 || length(grep('^clone$',fname)) != 0 || length(grep('^initialize$',fname)) != 0 ) { next }
+                
+                # Do some pretty formatting
+                name <- sub("_"," ",paste0(toupper(substring(fname, 1,1)), substring(fname, 2)))        
+                padding <- rep(" ", maxwidth-nchar(name))
+                cat (paste("\t",name)); cat(paste(padding,collapse='')); cat("\t: ");
+                
+                # Convert function name string to function
+                f <- eval(call("$",as.symbol('self'),as.symbol(fname)))
+                
+                # Call function
+                cat(paste(as.character(do.call(f, list())), "\n"));
+            }
+            cat ("\n")
+        },
+    
+        # Check for storage requirements
+        check_storage = function(){
+            quota_info <- list()
+            try(
+                quota_info <- suppressWarnings(strsplit(system('mmlsquota --block-size=G home 2>/dev/null',TRUE), "\\s+")),
+                silent=TRUE
+            )
+            
+            if (length(quota_info)>1) {
+                hquota <- suppressWarnings(as.integer(quota_info[[3]][[4]]))
+                husage <- suppressWarnings(as.integer(quota_info[[3]][[3]]))
+                #bquota <- as.integer(strsplit(system('check_quota bigdata',TRUE), "\\s+")[[3]][[4]])
 
-# Check for storage requirements
-myRenvCheck$check_storage <- function(){
-    quota_info <- suppressWarnings(strsplit(system('mmlsquota --block-size=G home',TRUE), "\\s+"))
-    hquota <- suppressWarnings(as.integer(quota_info[[3]][[4]]))
-    husage <- suppressWarnings(as.integer(quota_info[[3]][[3]]))
-    #bquota <- as.integer(strsplit(system('check_quota bigdata',TRUE), "\\s+")[[3]][[4]])
+                if (!is.na(hquota)){
+                    if (hquota - husage > 5) {
+                        #as.integer(bquota[[3]][[4]])-as.ingeger(bquota[[3]][[3]])>5
+                        return("Passed")
+                    }
+                }
+            }
+            return("Failed")
+        },
 
-    if (!is.na(hquota)){
-        if (hquota - husage > 5) {
-            #as.integer(bquota[[3]][[4]])-as.ingeger(bquota[[3]][[3]])>5
-            return("Passed")
+        # Check for library requirements
+        check_systempiper = function(){
+            Rlibs <- library()
+
+            # Check for systemPipeR library
+            if (match("systemPipeR",Rlibs$results)){
+                loaded <- suppressMessages(suppressWarnings(library(systemPipeR, quietly=TRUE)))
+                if (match("systemPipeR",loaded)){
+                    return("Passed")
+                }
+            }
+            return("Failed")
+        },
+
+        # Check for individually installed libraries
+        check_library = function(){
+            RlibsUser <- Sys.getenv("R_LIBS_USER")
+            Rlibs <- suppressMessages(suppressWarnings(library(lib.loc=RlibsUser)))
+            if (length(Rlibs$results)==0){
+                return("Passed")
+            } else {
+                return("Failed")
+            }
+        },
+
+        # Check default R library paths
+        check_default_library = function(){
+            RlibsUser <- Sys.getenv("R_LIBS_USER")
+            #RlibsPath<-.libPaths()
+            RlibsPath <- .Library
+            if (RlibsUser != RlibsPath){
+                return("Passed")
+            } else {
+                return("Failed")
+            }
+        },
+
+        # Check R path, "home"
+        check_r_home = function(){
+            Rhome <- Sys.getenv("R_HOME")
+            if (Rhome == "/opt/linux/centos/7.x/x86_64/pkgs/R/3.4.0/lib64/R"){
+                return("Passed")
+            } else {
+                return("Failed")
+            }
+        },
+
+        # Check if module system is functioning
+        check_modules = function() {
+            # Get list of available modules
+            modules <- list()
+            try(
+                modules <- suppressWarnings(strsplit(system("bash -c \"module avail 2>&1\"", TRUE), "\\s+")),
+                silent=TRUE
+            )
+            
+            find_module <- function(module){
+                grep(module, modules, perl=TRUE)
+            }
+            
+            # Check if expected modules are in available list
+            mlist <- c("slurm/19.05.0","bwa/0.7.17","hisat2/2.2.1")
+            library("BiocParallel")
+            found <- bplapply(mlist, find_module)
+            cat('\n ')
+            if (length(mlist[is.na(mlist[found != 0])])==0){
+                return("Passed")
+            }else {
+                return(paste("\t\tFailed::", mlist[is.na(mlist[found != 0])]))
+            }
+        },
+
+        # Check if user has a .html directory
+        check_html = function(){
+            if (file.exists("~/.html") && file.info("~/.html")$mode > as.octmode("754")){
+                return("Passed")
+            } else {
+                return("Failed")
+            }
+        },
+
+        # Check if sbatch works
+        check_sbatch = function(){
+            sbatch <- NA
+            try(
+                sbatch <- suppressWarnings(strsplit(system("bash -c 'which sbatch' 2>/dev/null", TRUE),"\\s+")),
+                silent=TRUE
+            )
+            if (length(sbatch)==1 && file.exists(sbatch[[1]])){
+                return("Passed")
+            } else {
+                return("Failed")
+            }
         }
-    }
-    return("Failed")
-}
-
-# Check for library requirements
-myRenvCheck$check_systempiper <- function(){
-    Rlibs <- library()
-
-    # Check for systemPipeR library
-    if (match("systemPipeR",Rlibs$results)){
-        loaded <- suppressMessages(suppressWarnings(library(systemPipeR, quietly=TRUE)))
-        if (match("systemPipeR",loaded)){
-            return("Passed")
-        }
-    }
-    return("Failed")
-}
-
-# Check for individually installed libraries
-myRenvCheck$check_library <- function(){
-    RlibsUser <- Sys.getenv("R_LIBS_USER")
-    Rlibs <- suppressMessages(suppressWarnings(library(lib.loc=RlibsUser)))
-    if (length(Rlibs$results)==0){
-        return("Passed")
-    } else {
-        return("Failed")
-    }
-}
-
-# Check default R library paths
-myRenvCheck$check_default_library <- function(){
-    RlibsUser <- Sys.getenv("R_LIBS_USER")
-    #RlibsPath<-.libPaths()
-    RlibsPath <- .Library
-    if (RlibsUser != RlibsPath){
-        return("Passed")
-    } else {
-        return("Failed")
-    }
-}
-
-# Check R path, "home"
-myRenvCheck$check_r_home <- function(){
-    Rhome <- Sys.getenv("R_HOME")
-    if (Rhome == "/opt/linux/centos/7.x/x86_64/pkgs/R/3.4.0/lib64/R"){
-        return("Passed")
-    } else {
-        return("Failed")
-    }
-}
-
-# Check if module system is functioning
-myRenvCheck$check_modules <- function() {
-    modulelist <- function(){
-        strsplit(system("bash -c \"module avail 2>&1\"", TRUE), "\\s+")
-    }
-    modules <- modulelist()
-    find_module <- function(module){
-        grep(module, modules, perl=TRUE)
-    }
-    mlist <- c("slurm/16.05.4","bwa/0.7.12","tophat/2.1.1")
-    library("BiocParallel")
-    found <- bplapply(mlist, find_module)
-    if (length(mlist[is.na(mlist[found != 0])])==0){
-        return("Passed")
-    }else {
-        return(paste("Failed::", mlist[is.na(mlist[found != 0])]))
-    }
-}
-
-# Check if user has a .html directory
-myRenvCheck$check_html <- function(){
-    if (file.exists("~/.html") && file.info("~/.html")$mode > as.octmode("754")){
-        return("Passed")
-    } else {
-        return("Failed")
-    }
-}
-
-# Check if sbatch works
-myRenvCheck$check_sbatch <- function(){
-    sbatch <- NA
-    try(
-        sbatch <- suppressWarnings(strsplit(system("bash -c 'which sbatch' 2>/dev/null", TRUE),"\\s+")),
-        silent=TRUE
     )
-    if (length(sbatch)==1 && file.exists(sbatch[[1]])){
-        return("Passed")
-    } else {
-        return("Failed")
-    }
-}
+)
 
 # Run all checks
 .onLoad <- function(libname, pkgname){
-    run_checks(myRenvCheck)
+    myChecks <- RenvCheck$new()
 }
